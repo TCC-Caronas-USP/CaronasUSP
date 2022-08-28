4d85d767ba898816c0eb4c0031d41b716cca4a05
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-08-28 15:45:24 -0300

    [TCC-77] Refatora app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,19 +3,71 @@ import 'package:caronas_usp/app/modules/historico/ui/historico_page.dart';
 import 'package:caronas_usp/app/modules/oferecer/ui/oferecer_page.dart';
 import 'package:caronas_usp/app/modules/profile/ui/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _selectedIndex = 0;
+
+  final pages = [
+    () => PegarPage(),
+    () => HistoricoPage(),
+    () => OferecerPage(),
+    () => ProfilePage()
+  ];
+
+  Widget getPage() {
+    // Navigator.of(context).push(MaterialPageRoute(
+    //     builder: (context) => pages[_selectedIndex]()));
+    return pages[_selectedIndex]();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Scaffold(
+          body: getPage(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.green[400],
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white38,
+            iconSize: 25,
+            showUnselectedLabels: false,
+            currentIndex: _selectedIndex,
+            onTap: (index) => setState(() => _selectedIndex = index),
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.thumb_up_rounded),
+                label: 'Pegar',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people_rounded),
+                label: 'Caronas',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.directions_car_filled),
+                label: 'Oferecer',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_rounded),
+                label: 'Perfil',
+              ),
+            ],
+          ),
+        ));
   }
 }
 
@@ -29,7 +81,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int _selectedIndex = 0;
 
   final screens = const [
@@ -54,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         iconSize: 25,
         showUnselectedLabels: false,
         currentIndex: _selectedIndex,
-        onTap: (index) => setState(() => _selectedIndex = index) ,
+        onTap: (index) => setState(() => _selectedIndex = index),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.thumb_up_rounded),
