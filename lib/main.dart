c7198b89c63f8eb07ecc820a8b1e621b25c57ac9
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-08-28 15:42:35 -0300

    [TCC-77] Cria BLoC de Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:caronas_usp/app/modules/profile/bloc/profile_bloc.dart';
+import 'package:caronas_usp/app/repositories/user_repository.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:async';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const App()); // TODO Trocar por MultiProvider
+  runApp(
+    MultiProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ProfileBloc(
+            UserRepository(),
+          ),
+        )
+      ],
+      child: App(),
+    ),
+  );
 }
-
-
