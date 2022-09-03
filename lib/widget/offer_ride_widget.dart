a87df0e2048aa07481e4e5fdaf3393d7fc1db874
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-09-03 12:52:13 -0300

    offer ride widget initial implementation, with date and ride associated with that date

--- lib/widget/offer_ride_widget.dart
+++ lib/widget/offer_ride_widget.dart
@@ -1,31 +1,86 @@
+import 'package:caronas_usp/model/ride.dart';
+import 'package:caronas_usp/utils/user_offered_rides.dart';
 import 'package:caronas_usp/widget/ride_infos_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class OfferRide extends StatelessWidget {
-  // final VoidCallback onClicked;
-
-  // const OfferRide({Key? key, required this.onClicked}) : super(key: key);
+class OfferRide extends StatefulWidget {
   const OfferRide({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-    padding: const EdgeInsets.symmetric(horizontal: 24),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const <Widget>[
-        SizedBox(height: 12,),
-        Text(
-          "23/08/2022",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          textAlign: TextAlign.center,
-          softWrap: true,
-        ),
-        SizedBox(height: 12,),
-        RideInfos(),
-        SizedBox(height: 12,),
-        RideInfos(),
-      ],
-    ),
-  );
+  State<OfferRide> createState() => _OfferRideState();
 }
 
+class _OfferRideState extends State<OfferRide> {
+  @override
+  Widget build(BuildContext context) {
+    const userOfferedRides = UserOfferedRides.myOfferedRides;
+    List<List<Ride>> dailyOfferedRides = rideDates(userOfferedRides);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ListView.builder(
+              scrollDirection: Axis.vertical,
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: dailyOfferedRides.length,
+              itemBuilder: (context, index) {
+                final item = dailyOfferedRides[index];
+                final date = item[0].rideDate.substring(0, 10);
+
+                return buildRideDailyOffered(date, item);
+              })
+        ],
+      ),
+    );
+  }
+
+  List<List<Ride>> rideDates(List<Ride> userOfferedRides) {
+    List<String> userOfferedRideDates = [];
+    for (final ride in userOfferedRides) {
+      var rideDate = ride.rideDate.substring(0, 10);
+      userOfferedRideDates.add(rideDate);
+    }
+    userOfferedRideDates = userOfferedRideDates.toSet().toList()..sort();
+
+    List<List<Ride>> ridesOfTheSameDay = [];
+    for (final userOfferedRideDate in userOfferedRideDates) {
+      List<Ride> rideOfTheSameDay = [];
+      for (final ride in userOfferedRides) {
+        if (userOfferedRideDate == ride.rideDate.substring(0, 10)) {
+          rideOfTheSameDay.add(ride);
+        }
+      }
+      ridesOfTheSameDay.add(rideOfTheSameDay);
+    }
+
+    return ridesOfTheSameDay;
+  }
+
+  Widget buildRideDailyOffered(String rideDate, List<Ride> userOfferedRides) {
+    DateFormat format = DateFormat("yyyy-MM-dd");
+    DateTime rideDateFormatted = format.parse(rideDate);
+    String rideYear = rideDateFormatted.year.toString().padLeft(4, "0");
+    String rideMonth = rideDateFormatted.month.toString().padLeft(2, "0");
+    String rideDay = rideDateFormatted.day.toString().padLeft(2, "0");
+
+    return Column(children: [
+      Text(
+        " $rideDay/$rideMonth/$rideYear",
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        textAlign: TextAlign.center,
+        softWrap: true,
+      ),
+      const SizedBox(
+        height: 6,
+      ),
+      for (var ride in userOfferedRides) RideInfos(rideInfos: ride),
+      const SizedBox(
+        height: 16,
+      ),
+    ]);
+  }
+}
