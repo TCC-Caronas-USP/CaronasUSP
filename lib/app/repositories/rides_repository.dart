2d2030adbb610883f7b3bea794c861d6938aed41
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-09-14 20:21:17 -0300

    update on ride model and rides repositories with mocked data

--- lib/app/repositories/rides_repository.dart
+++ lib/app/repositories/rides_repository.dart
@@ -3,76 +3,126 @@ import 'dart:async';
 import 'package:caronas_usp/model/ride.dart';
 import 'package:caronas_usp/model/user.dart';
 
+const driverUser = User(
+  imagePath:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  name: "João Souza",
+  email: "joao.souza@usp.br",
+  cpf: "000.000.000-00",
+  telefone: "(99) 99999-9999",
+  instituto: "FAU - Arquitetura",
+  veiculoModelo: "Ford Ka",
+  veiculoCor: "Prata",
+  veiculoMarca: "Ford",
+  veiculoPlaca: "ABC1D34",
+  caronasUtilizadas: 10,
+  caronasRealizadas: 0,
+  ranking: 4.9,
+);
+
+const consumerUser1 = User(
+  imagePath:
+      "https://images.unsplash.com/photo-1542740348-39501cd6e2b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  name: "Maria Clara",
+  email: "maria.clara@usp.br",
+  cpf: "000.000.000-00",
+  telefone: "(99) 99999-9999",
+  instituto: "FFLCH - História",
+  veiculoModelo: "",
+  veiculoCor: "",
+  veiculoMarca: "",
+  veiculoPlaca: "",
+  caronasUtilizadas: 23,
+  caronasRealizadas: 0,
+  ranking: 4.8,
+);
+
+const consumerUser2 = User(
+  imagePath:
+      "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+  name: "Marcos Antonio",
+  email: "marcos.antonio@usp.br",
+  cpf: "000.000.000-00",
+  telefone: "(99) 99999-9999",
+  instituto: "POLI - Engenharia Civil",
+  veiculoModelo: "",
+  veiculoCor: "",
+  veiculoMarca: "",
+  veiculoPlaca: "",
+  caronasUtilizadas: 5,
+  caronasRealizadas: 0,
+  ranking: 5.0,
+);
+
+const myOfferedRides = <Ride>[
+  Ride(
+      rideId: '0000-0000',
+      driverUsers: driverUser,
+      rideDestinyDatetime: '2022-07-20 17:00:00',
+      destinyPlace: "CEPEUSP",
+      rideSourceDatetime: '2022-07-20 15:30:00',
+      sourcePlace: "Osasco",
+      price: 4,
+      totalOccupation: 4,
+      currentOccupation: 2,
+      consumersUsers: [consumerUser1, consumerUser2]),
+  Ride(
+      rideId: '0000-0001',
+      driverUsers: driverUser,
+      rideDestinyDatetime: '2022-07-20 20:30:00',
+      destinyPlace: "Osasco",
+      rideSourceDatetime: '2022-07-20 19:00:00',
+      sourcePlace: "CEPEUSP",
+      price: 4,
+      totalOccupation: 4,
+      currentOccupation: 0,
+      consumersUsers: []),
+  Ride(
+      rideId: '0000-0002',
+      driverUsers: driverUser,
+      rideDestinyDatetime: '2022-07-21 08:00:00',
+      destinyPlace: "POLI",
+      rideSourceDatetime: '2022-07-21 06:30:00',
+      sourcePlace: "Osasco",
+      price: 5,
+      totalOccupation: 4,
+      currentOccupation: 1,
+      consumersUsers: [consumerUser1]),
+  Ride(
+      rideId: '0000-0003',
+      driverUsers: driverUser,
+      rideDestinyDatetime: '2022-07-21 17:00:00',
+      destinyPlace: "Osasco",
+      rideSourceDatetime: '2022-07-21 15:30:00',
+      sourcePlace: "POLI",
+      price: 5,
+      totalOccupation: 4,
+      currentOccupation: 2,
+      consumersUsers: [consumerUser1, consumerUser2]),
+  Ride(
+    rideId: '0000-0004',
+    driverUsers: driverUser,
+    rideDestinyDatetime: '2022-07-22 10:00:00',
+    destinyPlace: "POLI",
+    rideSourceDatetime: '2022-07-22 08:30:00',
+    sourcePlace: "Osasco",
+    price: 6.50,
+    totalOccupation: 4,
+    currentOccupation: 0,
+    consumersUsers: [],
+  ),
+];
+
 class RidesRepository {
   Future<List<List<Ride>>> getUserOfferedRides() async {
     // return UserGraphQLRequester.getUserOfferedRides(email); TODO implementar requester
     await Future.delayed(
         const Duration(seconds: 1)); // TODO remover sleep e user mock
 
-    const user = User(
-      imagePath:
-      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-      name: "João Souza",
-      email: "joao.souza@usp.br",
-      cpf: "000.000.000-00",
-      telefone: "(99) 99999-9999",
-      instituto: "FAU - Arquitetura",
-      veiculoModelo: "Ford Ka",
-      veiculoCor: "Prata",
-      veiculoMarca: "Ford",
-      veiculoPlaca: "ABC1D34",
-      caronasUtilizadas: 10,
-      caronasRealizadas: 0,
-      ranking: 4.9,
-    );
-
-    const myOfferedRides = <Ride>[
-      Ride(
-          driverUsers: user,
-          rideDate: '2022-07-20 17:00:00',
-          destinyPlace: "CEPEUSP",
-          sourcePlace: "Osasco",
-          price: 4,
-          totalOccupation: 4,
-          currentOccupation: 2),
-      Ride(
-          driverUsers: user,
-          rideDate: '2022-07-20 20:30:00',
-          destinyPlace: "Osasco",
-          sourcePlace: "CEPEUSP",
-          price: 4,
-          totalOccupation: 4,
-          currentOccupation: 0),
-      Ride(
-          driverUsers: user,
-          rideDate: '2022-07-21 08:00:00',
-          destinyPlace: "POLI",
-          sourcePlace: "Osasco",
-          price: 5,
-          totalOccupation: 4,
-          currentOccupation: 1),
-      Ride(
-          driverUsers: user,
-          rideDate: '2022-07-21 17:00:00',
-          destinyPlace: "Osasco",
-          sourcePlace: "POLI",
-          price: 5,
-          totalOccupation: 4,
-          currentOccupation: 2),
-      Ride(
-          driverUsers: user,
-          rideDate: '2022-07-22 10:00:00',
-          destinyPlace: "POLI",
-          sourcePlace: "Osasco",
-          price: 6.50,
-          totalOccupation: 4,
-          currentOccupation: 0),
-    ];
-
     List<List<Ride>> rideDates(List<Ride> userOfferedRides) {
       List<String> userOfferedRideDates = [];
       for (final ride in userOfferedRides) {
-        var rideDate = ride.rideDate.substring(0, 10);
+        var rideDate = ride.rideDestinyDatetime.substring(0, 10);
         userOfferedRideDates.add(rideDate);
       }
       userOfferedRideDates = userOfferedRideDates.toSet().toList()..sort();
@@ -81,7 +131,8 @@ class RidesRepository {
       for (final userOfferedRideDate in userOfferedRideDates) {
         List<Ride> rideOfTheSameDay = [];
         for (final ride in userOfferedRides) {
-          if (userOfferedRideDate == ride.rideDate.substring(0, 10)) {
+          if (userOfferedRideDate ==
+              ride.rideDestinyDatetime.substring(0, 10)) {
             rideOfTheSameDay.add(ride);
           }
         }
@@ -92,5 +143,18 @@ class RidesRepository {
     }
 
     return rideDates(myOfferedRides);
+  }
+
+  Future<Ride?> getRide(rideId) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    Ride? ride;
+    for (var myRide in myOfferedRides) {
+      if (myRide.rideId == rideId) {
+        ride = myRide;
+      }
+    }
+
+    return ride;
   }
 }
