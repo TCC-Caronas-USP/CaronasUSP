6be83345a7954d93d4cf92cffd1f6362fa4eeb93
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-09-30 21:10:57 -0300

    implementation of floating actions buttons: cancel and edit

--- lib/app/modules/detalhes/ui/detalhes_page.dart
+++ lib/app/modules/detalhes/ui/detalhes_page.dart
@@ -9,31 +9,30 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
-
 class DetalhesPage extends StatefulWidget {
   final String rideId;
+  final bool offer;
 
-  const DetalhesPage({Key? key, required this.rideId}) : super(key: key);
+  const DetalhesPage({Key? key, required this.rideId, this.offer = true})
+      : super(key: key);
 
   @override
-  State<DetalhesPage> createState() => _DetalhesPageState(rideId: rideId);
+  State<DetalhesPage> createState() => _DetalhesPageState();
 }
 
 class _DetalhesPageState extends State<DetalhesPage> {
   DetalhesBloc? _detalhesBloc;
-  final String rideId;
 
   Ride? ride;
   bool _loading = true;
-
-  _DetalhesPageState({required this.rideId});
+  bool? _canceled = false;
 
   @override
   void initState() {
     super.initState();
 
     _detalhesBloc = context.read<DetalhesBloc>();
-    _detalhesBloc!.add(FetchRide(rideId));
+    _detalhesBloc!.add(FetchRide(widget.rideId));
   }
 
   Future<void> _handleListener(
@@ -60,7 +59,57 @@ class _DetalhesPageState extends State<DetalhesPage> {
                     color: Colors.green,
                     size: 50.0,
                   )
-                : DetailsRide(ride: ride!,),
+                : DetailsRide(
+                    ride: ride!,
+                  ),
+            floatingActionButton: _loading
+                ? null
+                : widget.offer
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                            FloatingActionButton(
+                                heroTag: "btnCancel",
+                                onPressed: () => showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) =>
+                                        AlertDialog(
+                                          title: const Text(
+                                              "Deseja realmente cancelar a carona?",
+                                              textAlign: TextAlign.center),
+                                          content: const Text(
+                                              "Caso cancele os caronistas serão alertados.",
+                                              textAlign: TextAlign.center),
+                                          alignment: Alignment.center,
+                                          actions: <Widget>[
+                                            TextButton(
+                                              onPressed: () =>
+                                                  Navigator.pop(context, 'Não'),
+                                              child: const Text('Não'),
+                                            ),
+                                            TextButton(
+                                              onPressed: () {
+                                                // Navigator.of(context).push(
+                                                //     MaterialPageRoute(
+                                                //         builder: (context) =>
+                                                //             const OferecerPage()));
+                                              },
+                                              child: const Text('Sim',
+                                                  style: TextStyle(
+                                                      color: Colors.red)),
+                                            ),
+                                          ],
+                                        )),
+                                backgroundColor: Colors.red[400],
+                                child: const Icon(Icons.cancel)),
+                            const SizedBox(height: 6),
+                            FloatingActionButton(
+                                heroTag: "btnEdit",
+                                onPressed: () {},
+                                backgroundColor: Colors.green[400],
+                                child: const Icon(Icons.edit)),
+                          ])
+                    : null, // TODO inserir lógica de floating action buttons para outras telas
           );
         },
         listener: _handleListener);
