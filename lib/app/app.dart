472cf054d670840ac43758aed9c515b135ca5c02
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-10-17 22:54:03 -0300

    [login] Adiciona LoginPage ao app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,9 @@
+import 'package:caronas_usp/app/modules/login/ui/login_page.dart';
 import 'package:caronas_usp/app/modules/pegar/ui/pegar_page.dart';
 import 'package:caronas_usp/app/modules/historico/ui/historico_page.dart';
 import 'package:caronas_usp/app/modules/oferecer/ui/oferecer_page.dart';
 import 'package:caronas_usp/app/modules/profile/ui/profile_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -33,7 +35,18 @@ class _AppState extends State<App> {
           primarySwatch: Colors.green,
         ),
         home: Scaffold(
-          body: getPage(),
+          body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasData && FirebaseAuth.instance.currentUser != null) {
+                return getPage();
+              } else {
+                return const LoginPage();
+              }
+            },
+          ),
           bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
             backgroundColor: Colors.green[400],
