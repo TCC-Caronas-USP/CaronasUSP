899ed25d4b04326d04e1be939e2a38b4426079d9
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-10-18 22:59:36 -0300

    import criar_blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:caronas_usp/app/modules/criar/bloc/criar_bloc.dart';
 import 'package:caronas_usp/app/modules/detalhes/bloc/detalhes_bloc.dart';
 import 'package:caronas_usp/app/modules/historico/bloc/historico_bloc.dart';
 import 'package:caronas_usp/app/modules/login/bloc/login_bloc.dart';
@@ -12,8 +13,6 @@ import 'app/app.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:async';
-
-import 'app/modules/criar/bloc/criar_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
