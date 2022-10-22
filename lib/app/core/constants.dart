6c4e9ecbddbee565274ee5df09422cb33a065c4d
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-10-22 01:09:26 -0300

    [TCC-79] Melhora constants

--- lib/app/core/constants.dart
+++ lib/app/core/constants.dart
@@ -1,4 +1,12 @@
-const String DOMAIN = 'http://localhost:8551';
+const String DEV_URL = '10.0.2.2:8551';
+const String HOMOL_URL = 'caronasuspuserservice-production.up.railway.app';
+const String PROD_URL = 'caronasuspuserservice-production.up.railway.app';
+
+const String SERVER_URL = DEV_URL;
+const String SERVER_HOST = "10.0.2.2";
+const int SERVER_PORT = 8551;
+
+const String ALLOWED_EMAIL_DOMAIN = 'usp.br';
 
 const String TIME_ZONE = 'America/Sao_Paulo';
 
