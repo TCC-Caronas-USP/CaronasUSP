200f5ff5f43cae758f3b7b4d4f8384463af04518
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-10-21 21:55:00 -0300

    json anotation files

---
+++ lib/model/location.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Location _$LocationFromJson(Map<String, dynamic> json) => Location(
+      id: json['id'] as String,
+      description: json['description'] as String,
+      lat: (json['lat'] as num).toDouble(),
+      lon: (json['lon'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'id': instance.id,
+      'description': instance.description,
+      'lat': instance.lat,
+      'lon': instance.lon,
+    };
