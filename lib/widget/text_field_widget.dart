5d038721772ebaabc604c2d11d7f6163874879c0
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-10-21 22:00:28 -0300

    adaptation on field widget, because the controller needs to be on the father context

--- lib/widget/text_field_widget.dart
+++ lib/widget/text_field_widget.dart
@@ -7,67 +7,50 @@ class TextFieldWidget extends StatefulWidget {
   final String? Function(String?)? onValidation;
   final TextEditingController? fieldController;
   final FocusNode? focusNode;
+  final Icon? suffixIcon;
   final TextInputType textInputType;
   final String type;
   final bool enabled;
 
-  TextFieldWidget({
-    Key? key,
-    required this.label,
-    this.text,
-    this.onChanged,
-    this.onValidation,
-    this.fieldController,
-    this.focusNode,
-    this.textInputType = TextInputType.name,
-    this.type = "text",
-    this.enabled = true
-  }) : super(key: key);
+  TextFieldWidget(
+      {Key? key,
+      required this.label,
+      this.text,
+      this.onChanged,
+      this.onValidation,
+      this.fieldController,
+      this.focusNode,
+      this.suffixIcon,
+      this.textInputType = TextInputType.name,
+      this.type = "text",
+      this.enabled = true})
+      : super(key: key);
 
   @override
   State<TextFieldWidget> createState() => _TextFieldWidgetState();
 }
 
 class _TextFieldWidgetState extends State<TextFieldWidget> {
-  late final TextEditingController controller;
 
   @override
   void initState() {
     super.initState();
-    controller =
-        widget.fieldController ?? TextEditingController(text: widget.text);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => TextFormField(
-    enabled: widget.enabled,
-    autovalidateMode: AutovalidateMode.onUserInteraction,
-    validator: widget.onValidation,
-    onChanged: widget.onChanged ?? onChanged,
-    controller: controller,
-    keyboardType: widget.textInputType,
-    decoration: InputDecoration(
-        border: const UnderlineInputBorder(),
-        labelText: widget.label,
-        suffixIcon: controller.text.isNotEmpty
-            ? IconButton(
-          onPressed: () {
-            setState(() {
-              widget.text = null;
-              controller.clear();
-            });
-          },
-          icon: const Icon(Icons.clear_outlined, color: Colors.green),
-        )
-            : null),
-  );
+        enabled: widget.enabled,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: widget.onValidation,
+        onChanged: widget.onChanged ?? onChanged,
+        controller: widget.fieldController,
+        keyboardType: widget.textInputType,
+        decoration: InputDecoration(
+          border: const UnderlineInputBorder(),
+          labelText: widget.label,
+          suffixIcon: widget.suffixIcon,
+        ),
+      );
 
   void onChanged(String value) {
     setState(() {
